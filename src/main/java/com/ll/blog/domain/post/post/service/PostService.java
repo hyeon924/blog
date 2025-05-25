9e0fa29a433099b3672547bb67437ab416d246e7
hyeon924 <hyeonn4713@gmail.com>
2025-05-25 21:53:09 +0900

    post 03 - service

---
+++ src/main/java/com/ll/blog/domain/post/post/service/PostService.java
@@ -0,0 +1,11 @@
+package com.ll.blog.domain.post.post.service;
+
+import com.ll.blog.domain.post.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+}
