9a1a6afe6f14e190ff211fb88c633c7116c588a9
admin <hyeon0924@hepi.co.kr>
2025-05-27 14:49:08 +0900

    repository

--- src/main/java/com/ll/blog/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/ll/blog/domain/post/post/repository/PostRepository.java
@@ -2,6 +2,11 @@ package com.ll.blog.domain.post.post.repository;
 
 import com.ll.blog.domain.post.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+// post 엔티티를 DB와 연결하는 JPA Repository 인터페이스
+// Spring Data JPA가 구현체를 자동으로 만들어줌
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+//    기본적인 CRUD 메서드는 JapRepository가 다 제공함
 }
